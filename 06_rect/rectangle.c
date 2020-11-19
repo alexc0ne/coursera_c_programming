0b9e92323c94ff484d32f8eb11b5997ba4da65d0
Coursera Learner <nobody@nowhere.nul>
2020-11-19 01:21:15 +0000

    Did assignment 6

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -1,32 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+
 //I've provided "min" and "max" functions in
 //case they are useful to you
-int min (int a, int b) {
-  if (a < b) {
-    return a;
-  }
-  return b;
+int min (int a, int b)
+{
+    if (a < b) return a;
+    return b;
 }
-int max (int a, int b) {
-  if (a > b) {
-    return a;
-  }
-  return b;
+int max (int a, int b)
+{
+    if (a > b) return a;
+    return b;
 }
+
+
 
 //Declare your rectangle structure here!
-
-
-rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
-  return r;
+typedef struct rectangle_t
+{
+    int x, y, width, height;
 }
-rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+rectangle;
+
+
+
+rectangle canonicalize(rectangle r)
+{
+    if (r.width < 0)
+    {
+        r.x += r.width;
+        r.width *= -1;
+    }
+
+    if (r.height < 0)
+    {
+        r.y += r.height;
+        r.height *= -1;
+    }
+
+    return r;
 }
 
+
+typedef struct segment_t
+{
+    int left, right;
+}
+segment;
+
+
+
+segment get_segment(int x1, int x2, int x3, int x4)
+{
+    segment s;
+    
+    if (x1 > x4 || x2 < x3)
+    {
+        s.left = s.right = 0;
+        return s;
+    }
+    
+    int tmp = 0;
+    int arr[4] = {x1, x2, x3, x4};
+    for (int i = 0; i < 3; ++i)
+        for (int j = i + 1; j < 4; ++j)
+            if (arr[i] > arr[j])
+            {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+    s.left = arr[1];
+    s.right = arr[2];    
+
+    return s;
+}
+
+
+rectangle intersection(rectangle r1, rectangle r2)
+{
+    r1 = canonicalize(r1);
+    r2 = canonicalize(r2);
+    rectangle r;
+
+    if (r1.x + r1.width < r2.x || r2.x + r2.width < r1.x)
+    {
+        r.x = r.y = r.width = r.height = 0;
+        return r;
+    }
+
+    if (r1.y + r1.height < r2.y || r2.y + r2.height < r1.y)
+    {
+        r.x = r.y = r.width = r.height = 0;
+        return r;
+    }
+
+    segment x_segment = get_segment(r1.x, r1.x + r1.width, r2.x, r2.x + r2.width);
+    segment y_segment = get_segment(r1.y, r1.y + r1.height, r2.y, r2.y + r2.height);
+    
+    r.x = x_segment.left;
+    r.y = y_segment.left;
+    r.width = x_segment.right - x_segment.left;
+    r.height = y_segment.right - y_segment.left;
+
+    return r;
+}
+
+
+//======================================================
 //You should not need to modify any code below this line
 void printRectangle(rectangle r) {
   r = canonicalize(r);
