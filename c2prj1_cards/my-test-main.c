e36c385c0f6ca294e0d969927e04400666e9ae91
Coursera Learner <nobody@nowhere.nul>
2020-11-24 00:16:21 +0000

    3rd try

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "cards.h"
 
-int main(void) {
-
+int main(void)
+{   
+    print_card(card_from_letters('0', 's'));
+    printf("\n");
+    print_card(card_from_letters('0', 'h'));
+    printf("\n");
+    print_card(card_from_letters('0', 'd'));
+    printf("\n");
+    print_card(card_from_letters('0', 'c'));
+    printf("\n");
+    
+    return EXIT_SUCCESS;
 }
